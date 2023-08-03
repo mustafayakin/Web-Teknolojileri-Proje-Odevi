5069ed75451b88d94f904c5ec2ffd21c4fe6b93d
Mustafa Yakın <mustafa.yakin@ogr.sakarya.edu.tr>
2023-08-03 22:10:12 +0300

    css kodlarında değişiklikler
    scroll-behavior:smooth kullanıldı. api'de kullanılmak üzere ilgi alanım sayfasının tasarımında ve scriptinde bir takım değiştirmeler yapıldı

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-var attempt = 3; //Giriş hakkı sayısı
+
 function loginkontrol() {
-    let mesajsc = "Giriş Başarılı! Hoşgeldin Mustafa";
     var pass = document.forms["girisYap_form"]["sifre"].value;
     if (pass == "") {
         alert("Şifre Boş Bırakılamaz!");
         return false;
     }
     var email = document.forms["girisYap_form"]["mail"].value;
-    var forMail = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+.)+([.])+[a-zA-Z0-9.-]{2,4}$/;
+    var forMail = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+.)+([.])+[a-zA-Z0-9.-]{2,4}$/; // how to do an email validation in javascript ??
     if (forMail.test(email) == false) {
         alert("Geçersiz Mail Adresi!!!");
         return false;
     }
 }
+function iletisimkontrol(){
+
+}
+
+// Yukarı butonu bul
+let mybutton = document.getElementById("yukariBtn");
+
+// 20 piksel aşağı inince scroll fonksiyonunu çalıştır
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// kullanıcı butona tıklayınca üste gitmesini sağlayan fonksiyon.
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function ilgialanimsub(){
+  alert("Gonderdiniz!");
+  var yazan;
+  var adres = document.getElementById("ilgialanimtextid"); 
+  yazan = adres.value;
+  adres.value ="";
+  document.getElementById("yazilan").innerHTML = yazan;
+
+}
+
